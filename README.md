e1e44d0939da93c6a066501947c31afaea6dc2fb
Raphael Abreu <raphaelrat@hotmail.com>
2022-07-14 09:00:21 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# On Fly
+
+App para disciplina Planejamento e Gestão de Projetos do curso de Engenharia de Computação da UFSC.
---
+++ android/app/src/main/kotlin/com/example/onfly/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.onfly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
