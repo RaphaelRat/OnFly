1ced60745ad9ef38633443089aade1788c599c9a
Raphael Abreu <raphaelrat@hotmail.com>
2022-07-15 00:07:05 -0300

    add quantidadeDrone in user model

--- lib/app/data/model/user/user.dart
+++ lib/app/data/model/user/user.dart
@@ -2,21 +2,25 @@ class User {
   String nome;
   String email;
   String senha;
+  int quantidadeDrone;
   User({
     required this.nome,
     required this.email,
     required this.senha,
+    required this.quantidadeDrone,
   });
 
   User copyWith({
     String? nome,
     String? email,
     String? senha,
+    int? quantidadeDrone,
   }) {
     return User(
       nome: nome ?? this.nome,
       email: email ?? this.email,
       senha: senha ?? this.senha,
+      quantidadeDrone: quantidadeDrone ?? this.quantidadeDrone,
     );
   }
 
@@ -25,6 +29,7 @@ class User {
       'nome': nome,
       'email': email,
       'senha': senha,
+      'quantidadeDrone': quantidadeDrone,
     };
   }
 
@@ -33,19 +38,24 @@ class User {
       nome: map['nome'] ?? '',
       email: map['email'] ?? '',
       senha: map['senha'] ?? '',
+      quantidadeDrone: map['quantidadeDrone']?.toInt() ?? 0,
     );
   }
 
   @override
-  String toString() => 'User(nome: $nome, email: $email, senha: $senha)';
+  String toString() {
+    return 'User(nome: $nome, email: $email, senha: $senha, quantidadeDrone: $quantidadeDrone)';
+  }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is User && other.nome == nome && other.email == email && other.senha == senha;
+    return other is User && other.nome == nome && other.email == email && other.senha == senha && other.quantidadeDrone == quantidadeDrone;
   }
 
   @override
-  int get hashCode => nome.hashCode ^ email.hashCode ^ senha.hashCode;
+  int get hashCode {
+    return nome.hashCode ^ email.hashCode ^ senha.hashCode ^ quantidadeDrone.hashCode;
+  }
 }
--- lib/app/data/model/user/user_mock.dart
+++ lib/app/data/model/user/user_mock.dart
@@ -4,16 +4,19 @@ final userRaphael = User(
   email: 'raphael@gmail.com',
   senha: '123123',
   nome: 'Raphael Abreu',
+  quantidadeDrone: 4,
 );
 final userNicolas = User(
   email: 'nicolas@gmail.com',
   senha: '123123',
   nome: 'Nícolas Baümle',
+  quantidadeDrone: 6,
 );
 final userMatheus = User(
   email: 'matheus@gmail.com',
   senha: '123123',
   nome: 'Matheus Kolln',
+  quantidadeDrone: 2,
 );
 
 final users = [
