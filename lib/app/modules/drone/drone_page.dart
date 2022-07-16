e61f61cb026316c26fed64791116f8f5d8bb1d98
Raphael Abreu <raphaelrat@hotmail.com>
2022-07-15 21:17:34 -0300

    fix layout on DronePage

--- lib/app/modules/drone/drone_page.dart
+++ lib/app/modules/drone/drone_page.dart
@@ -46,92 +46,100 @@ class DronePage extends GetView<DroneController> {
               overscroll.disallowIndicator();
               return false;
             },
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  droneCard(controller.drone, isDronePage: true),
-                  const SizedBox(height: 24),
-                  const Divider(thickness: 1, height: 0),
-                  Container(
-                    width: double.infinity,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
-                      color: AppColors.whiteSmoke,
-                    ),
-                    child: Form(
-                      key: droneFormKey,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const SizedBox(height: 24),
-                            Center(
-                              child: Text('Preencha com os dados da entrega', style: textTheme.headline6?.copyWith(fontWeight: FontWeight.w600)),
-                            ),
-                            const SizedBox(height: 36),
-                            Text('De onde a entrega está saindo', style: textTheme.subtitle1?.copyWith(fontWeight: FontWeight.w400)),
-                            const SizedBox(height: 8),
-                            TextFormField(
-                              decoration: getTextFieldDecoration(context: context, enabled: true, label: 'Partida', isInvalid: false),
-                              validator: (partida) => partida == null || partida.isEmpty ? 'Campo obrigatório' : null,
-                              onSaved: controller.onPartidaFormChanged,
-                              autovalidateMode: AutovalidateMode.onUserInteraction,
-                            ),
-                            const SizedBox(height: 24),
-                            Text('Para onde a entrega está indo', style: textTheme.subtitle1?.copyWith(fontWeight: FontWeight.w400)),
-                            const SizedBox(height: 8),
-                            TextFormField(
-                              decoration: getTextFieldDecoration(context: context, enabled: true, label: 'Chegada', isInvalid: false),
-                              validator: (chegada) => chegada == null || chegada.isEmpty ? 'Campo obrigatório' : null,
-                              onSaved: controller.onChegadaFormChanged,
-                              autovalidateMode: AutovalidateMode.onUserInteraction,
-                            ),
-                            const SizedBox(height: 24),
-                            Text('Conteúdo da entrega', style: textTheme.subtitle1?.copyWith(fontWeight: FontWeight.w400)),
-                            const SizedBox(height: 8),
-                            TextFormField(
-                              decoration: getTextFieldDecoration(context: context, enabled: true, label: 'Conteúdo', isInvalid: false),
-                              validator: (conteudo) => conteudo == null || conteudo.isEmpty ? 'Campo obrigatório' : null,
-                              onSaved: controller.onChegadaFormChanged,
-                              autovalidateMode: AutovalidateMode.onUserInteraction,
-                            ),
-                            const SizedBox(height: 24),
-                            Text('Peso do conteúdo', style: textTheme.subtitle1?.copyWith(fontWeight: FontWeight.w400)),
-                            const SizedBox(height: 8),
-                            TextFormField(
-                                decoration: getTextFieldDecoration(context: context, enabled: true, label: 'Peso', isInvalid: false),
-                                validator: pesoValidator,
-                                onSaved: controller.onPesoFormChanged,
-                                autovalidateMode: AutovalidateMode.onUserInteraction,
-                                keyboardType: TextInputType.number),
-                            const SizedBox(height: 24),
-                            SizedBox(
-                              height: 48,
-                              child: Obx(
-                                () => AnimatedSwitcher(
-                                  duration: const Duration(milliseconds: 200),
-                                  child: controller.isLoading.value
-                                      ? const Center(child: CircularProgressIndicator())
-                                      : SizedBox(
-                                          width: double.infinity,
-                                          child: ElevatedButton(
-                                            style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(AppColors.indigoA700)),
-                                            child: const Text('Enviar'),
-                                            onPressed: () => onEnviarPressed(droneFormKey),
-                                          ),
-                                        ),
-                                ),
+            child: CustomScrollView(
+              slivers: [
+                SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: Column(
+                    children: [
+                      droneCard(controller.drone, isDronePage: true),
+                      const SizedBox(height: 24),
+                      const Divider(thickness: 1, height: 0),
+                      Expanded(
+                        child: Container(
+                          width: double.infinity,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+                            color: AppColors.whiteSmoke,
+                          ),
+                          child: Form(
+                            key: droneFormKey,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const SizedBox(height: 24),
+                                  Center(
+                                    child:
+                                        Text('Preencha com os dados da entrega', style: textTheme.headline6?.copyWith(fontWeight: FontWeight.w600)),
+                                  ),
+                                  const SizedBox(height: 36),
+                                  Text('De onde a entrega está saindo', style: textTheme.subtitle1?.copyWith(fontWeight: FontWeight.w400)),
+                                  const SizedBox(height: 8),
+                                  TextFormField(
+                                    decoration: getTextFieldDecoration(context: context, enabled: true, label: 'Partida', isInvalid: false),
+                                    validator: (partida) => partida == null || partida.isEmpty ? 'Campo obrigatório' : null,
+                                    onSaved: controller.onPartidaFormChanged,
+                                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                                  ),
+                                  const SizedBox(height: 24),
+                                  Text('Para onde a entrega está indo', style: textTheme.subtitle1?.copyWith(fontWeight: FontWeight.w400)),
+                                  const SizedBox(height: 8),
+                                  TextFormField(
+                                    decoration: getTextFieldDecoration(context: context, enabled: true, label: 'Chegada', isInvalid: false),
+                                    validator: (chegada) => chegada == null || chegada.isEmpty ? 'Campo obrigatório' : null,
+                                    onSaved: controller.onChegadaFormChanged,
+                                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                                  ),
+                                  const SizedBox(height: 24),
+                                  Text('Conteúdo da entrega', style: textTheme.subtitle1?.copyWith(fontWeight: FontWeight.w400)),
+                                  const SizedBox(height: 8),
+                                  TextFormField(
+                                    decoration: getTextFieldDecoration(context: context, enabled: true, label: 'Conteúdo', isInvalid: false),
+                                    validator: (conteudo) => conteudo == null || conteudo.isEmpty ? 'Campo obrigatório' : null,
+                                    onSaved: controller.onChegadaFormChanged,
+                                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                                  ),
+                                  const SizedBox(height: 24),
+                                  Text('Peso do conteúdo', style: textTheme.subtitle1?.copyWith(fontWeight: FontWeight.w400)),
+                                  const SizedBox(height: 8),
+                                  TextFormField(
+                                      decoration: getTextFieldDecoration(context: context, enabled: true, label: 'Peso', isInvalid: false),
+                                      validator: pesoValidator,
+                                      onSaved: controller.onPesoFormChanged,
+                                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                                      keyboardType: TextInputType.number),
+                                  const SizedBox(height: 24),
+                                  SizedBox(
+                                    height: 48,
+                                    child: Obx(
+                                      () => AnimatedSwitcher(
+                                        duration: const Duration(milliseconds: 200),
+                                        child: controller.isLoading.value
+                                            ? const Center(child: CircularProgressIndicator())
+                                            : SizedBox(
+                                                width: double.infinity,
+                                                child: ElevatedButton(
+                                                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(AppColors.indigoA700)),
+                                                  child: const Text('Enviar'),
+                                                  onPressed: () => onEnviarPressed(droneFormKey),
+                                                ),
+                                              ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 24),
+                                ],
                               ),
                             ),
-                            const SizedBox(height: 24),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
